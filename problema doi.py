58001e1d2282ada5eae6411ce38ce67df1dab854
terintelivia <70704787+terintelivia@users.noreply.github.com>
2020-10-18 20:34:37 +0300

    Add files via upload

---
+++ problema doi.py
@@ -0,0 +1,7 @@
+a = int(input(" dati primul  numar"))
+b = int(input("dati al doilea numar"))
+c = int(input("dati al treilea numar"))
+if ((a<a+b) and (b<c+a) and (c<a+b)):
+    print("da")
+else:
+    print("nu")
